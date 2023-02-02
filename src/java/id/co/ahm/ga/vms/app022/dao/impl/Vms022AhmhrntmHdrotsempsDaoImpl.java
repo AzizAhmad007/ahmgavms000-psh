f5754fc40f27ae80ec7d553aef17e68a5bbe16ab
reza.mr <reza.m.ramadhan@intra.astra-honda.com>
2023-02-02 10:35:49 +0700

    VMS022: try sout true false

--- src/java/id/co/ahm/ga/vms/app022/dao/impl/Vms022AhmhrntmHdrotsempsDaoImpl.java
+++ src/java/id/co/ahm/ga/vms/app022/dao/impl/Vms022AhmhrntmHdrotsempsDaoImpl.java
@@ -228,6 +228,18 @@ public class Vms022AhmhrntmHdrotsempsDaoImpl extends HrHibernateDao<AhmhrntmHdro
                 Vms022VoMonitoring vo = new Vms022VoMonitoring();
 
                 boolean filterData = vms022AhmhrntmMstpicotsDao.isPicAvailable(userId, obj[8] + "", obj[3] + "");
+                
+                System.out.println("===========================================================================");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("True or false using role = " + role);
+                System.out.println(filterData);
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("===========================================================================");
+                
                 if (filterData == false && role.equalsIgnoreCase("RO_GAVMS_PICAHM")) {
                     continue;
                 }
