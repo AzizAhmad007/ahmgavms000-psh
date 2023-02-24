609c18c2d6cdb563888c84b3ec23d92dfcdd1dcc
reza.mr <reza.m.ramadhan@intra.astra-honda.com>
2023-02-24 09:27:54 +0700

    VMS022: adding save params

--- src/java/id/co/ahm/ga/vms/app022/dao/impl/Vms022AhmitwfsMstwfdochistDaoImpl.java
+++ src/java/id/co/ahm/ga/vms/app022/dao/impl/Vms022AhmitwfsMstwfdochistDaoImpl.java
@@ -8,6 +8,7 @@ import id.co.ahm.ga.vms.app000.model.wfs.AhmitwfsMstwfdochist;
 import id.co.ahm.ga.vms.app022.dao.Vms022AhmitwfsMstwfdochistDao;
 import id.co.ahm.jxf.dao.WfsHibernateDao;
 import java.io.Serializable;
+import java.util.Random;
 import org.hibernate.SQLQuery;
 import org.springframework.stereotype.Repository;
 
@@ -22,10 +23,31 @@ public class Vms022AhmitwfsMstwfdochistDaoImpl extends WfsHibernateDao<AhmitwfsM
     public Boolean generateHistory(String seq, String username, String outId, String wflow, String hist) {
         try {
 
+            String WF = "WF16771471";
+
+            Random random = new Random();
+            int randomNumber = random.nextInt(100000);
+            String formattedNumber = String.format("%05d", randomNumber);
+            
+            WF = WF + formattedNumber;
+
             java.util.Date datenow = new java.util.Date();
             int yearnow = datenow.getYear() + 1900;
             int monthnow = datenow.getMonth() + 1;
             int daynow = datenow.getDay();
+
+            SQLQuery q1 = getCurrentSession().createSQLQuery("IN"
+                    + "SERT INTO AHMITWFS_MSTWFDOCSTAT (VWFGUID, VWFID, VWFVERSION, VDOCID, VCREA, DCREA) "
+                    + "VALUES("
+                    + "\'" + wflow + "\',"
+                    + "\'" + WF + "\',"
+                    + "1 ,"
+                    + "\'" + seq + "\',"
+                    + "\'" + username + "\',"
+                    + "\'" + monthnow + "-" + daynow + "-" + yearnow + "\'"
+                    + ")"
+            );
+            q1.executeUpdate();
 
             SQLQuery q = getCurrentSession().createSQLQuery("IN"
                     + "SERT INTO AHMITWFS_MSTWFDOCHIST (VWFGUID, VHISTID, VTASKID, VEVENTTYPE, VTASKRESULT, VNOTE, VDOCID, VCREA, DCREA)"
