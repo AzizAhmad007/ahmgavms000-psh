f3255043a68c132ae9f750bc264b313638a2f791
reza.mr <reza.m.ramadhan@intra.astra-honda.com>
2023-04-18 12:20:39 +0700

    VMS022: try fix excel

--- src/java/id/co/ahm/ga/vms/app022/rest/Vms022Rest.java
+++ src/java/id/co/ahm/ga/vms/app022/rest/Vms022Rest.java
@@ -261,9 +261,9 @@ public class Vms022Rest {
         DtoParamPaging dtoParam = new DtoParamPaging();
         dtoParam.setOffset(0);
         dtoParam.setLimit(0);
-        dtoParam.setSort(null);
+        dtoParam.setSort("desc");
         dtoParam.setSearch(search);
-        dtoParam.setOrder("");
+        dtoParam.setOrder("outId");
  
         DtoResponseWorkspace dtoResponseWorkspace = vms022Service.getExcel(dtoParam);
         List<Vms022VoMonitoring> vms022VoMonitor = (List<Vms022VoMonitoring>) dtoResponseWorkspace.getData();
