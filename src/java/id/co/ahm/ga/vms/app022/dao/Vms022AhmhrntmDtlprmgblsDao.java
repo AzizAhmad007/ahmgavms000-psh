44c95975507c7ebdff346e1602abf3ef074e57f9
reza.mr <reza.m.ramadhan@intra.astra-honda.com>
2023-05-04 08:49:33 +0700

    VMS022: Try Fix Query error (Error on display plant in main data)

--- src/java/id/co/ahm/ga/vms/app022/dao/Vms022AhmhrntmDtlprmgblsDao.java
+++ src/java/id/co/ahm/ga/vms/app022/dao/Vms022AhmhrntmDtlprmgblsDao.java
@@ -21,7 +21,7 @@ public interface Vms022AhmhrntmDtlprmgblsDao extends HrDao<AhmhrntmDtlprmgbls, S
     
     public List<Vms022VoLov> getPlant(String outid, String nik);
     
-    public String getPlantForExcel(String outid, String nik);
+    public String getPlantForExcel(String outid, String nik, String nrp);
     
     public String getPlantIDForExcel(String outid, String nik);
     
